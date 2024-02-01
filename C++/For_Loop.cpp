718ebbae31fb8e24761bdbadaa0a68f2730c8908
Novascone <54559468+Novascone@users.noreply.github.com>
2024-02-01 10:09:57 -0700

    for loop

---
+++ C++/For_Loop.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    char *nums[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    int a;
+    int b;
+    cin >> a >> b;
+    
+    for(int i = a; i <= b; i++){
+        if(i >= 1 and i <= 9) {
+        printf("%s\n", nums[i-1]);
+        }
+        else if(i > 9) {
+             if(i % 2 == 0){
+                printf("%s\n", "even"); 
+             }
+             else{
+                printf("%s\n", "odd");
+             }
+            
+        }
+       
+        
+    }
+    
+    return 0;
+}
